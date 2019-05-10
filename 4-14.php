accf225ec8a8efc5dd48ff4b3237ba06d9e38035
Hsu Htet <hsu.htet.aung@digitallaboratory.net>
2019-05-10 14:55:59 +0630

    card-21320891 - PHP Ch4

---
+++ 4-14.php
@@ -0,0 +1,14 @@
+<?php
+$letters[0]='A';
+$letters[1]='B';
+$letters[3]='D';
+$letters[2]='C';
+//foreach($letters as $letter)
+//{
+//	print $letter;
+//}
+for($i=0, $num_letters=count($letters);$i<$num_letters;$i++)
+{
+	print $letters[$i];
+}
+?>
