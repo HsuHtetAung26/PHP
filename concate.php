6338caadec3a527340a3af22a6be03088efcc81f
Hsu Htet <hsu.htet.aung@digitallaboratory.net>
2019-05-15 09:01:06 +0630

    card21534026 - commit

--- concate.php
+++
@@ -1,4 +0,0 @@
-<?php
-print 'bread'.'fruit'; 
-print "It's a beautiful day".'in the neighborhood';
-?>
--- strtolow.php
+++
@@ -1,5 +0,0 @@
-<?php
-print strtolower('Beef, CHICKEN, Pork, duCK'); 
-echo "<br>";
-print strtoupper('Beef, CHICKEN,Pork,duCK');
-?>
