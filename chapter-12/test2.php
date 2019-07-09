4b22a59f0ed205f7872eb06a092e33ae2c1e5e8e
khinsweswewin <35557393+khinsweswewin@users.noreply.github.com>
2019-07-09 10:01:28 +0630

    #card-23725019 - chapter12 review

--- chapter-12/test2.php
+++ chapter-12/test2.php
@@ -2,7 +2,7 @@
 $reader = new XMLReader();
 $reader->open( __DIR__ . '/card-catalog.xml' );
 while ( $reader->read() ) {
-	if( $reader->nodeType==XMLREADER::ELEMENT && $reader->localName=='author' ) {
+	if( $reader->nodeType == XMLREADER::ELEMENT && $reader->localName == 'author' ) {
 		$reader->read();
 		print $reader->value . '<br>';
 	}
