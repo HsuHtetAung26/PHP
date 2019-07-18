544f62d86c64e5e19220825cb62fd34b0e3bdbbe
khinsweswewin <35557393+khinsweswewin@users.noreply.github.com>
2019-07-18 10:05:26 +0630

    #card-24104793 - chapter(16) review

--- chapter-16/test-3.php
+++ chapter-16/test-3.php
@@ -4,7 +4,7 @@ ldap_bind( $ds ) or die( $php_errormsg );
 $sr = ldap_search( $ds, 'o=Example Inc., c=US', 'sn=*' ) or die( $php_errormsg );
 $e = ldap_get_entries( $ds, $sr ) or die( $php_errormsg );
 for ( $i=0; $i < $e['count']; $i++ ) {
-echo $info[ $i ]['cn'][0] . ' (' . $info[ $i ]['mail'][0] . ')<br>';
+	echo $info[ $i ]['cn'][0] . ' (' . $info[ $i ]['mail'][0] . ')<br>';
 }
 ldap_close( $ds ) or die( $php_errormsg );
 ?>
